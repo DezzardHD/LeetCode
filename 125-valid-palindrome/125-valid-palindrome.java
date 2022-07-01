4f86f07316ba5ba936177f839373fb236472968b
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2022-07-01 23:18:55 +0200

    Time: 5 ms (75.79%), Space: 43.3 MB (75.52%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        
+        int a = 0;
         int tInner = s.length() - 1;
         for (int i = 0; i < s.length(); i++){
             if (Character.isLetterOrDigit(s.charAt(i))){
