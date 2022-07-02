4c5326391aa519811fd9dda2ffa351befb31dd9e
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2022-07-02 19:55:41 +0200

    Time: 0 ms (100.00%), Space: 41.1 MB (34.16%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,17 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        double result = 0;
-        for (int i = 0; i <= n / 2; i++){
-            result += factorial(n - i) / factorial(n - 2 * i) / factorial(i);
-        }
-        return (int) result;
-    }
-    
-    private double factorial(int n){
-        double result = 1; 
-        for (int i = n; i > 0; i--){
-            result *= i;
-        }
-        return result;
+        double a = 1;
+        double b = 1;
+            while(n > 1){
+                double tmp = a;
+                a += b;
+                b = tmp;
+                n--;
+            }
+        return (int) a;
     }
 }
