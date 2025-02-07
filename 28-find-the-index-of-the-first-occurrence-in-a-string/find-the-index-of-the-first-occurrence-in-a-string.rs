a83f87c6973371c95bdbb23b4b352a1e7ddf4e33
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-02-07 19:04:56 +0100

    Time: 0 ms (100.00%) | Memory: 2.2 MB (60.60%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.rs
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.rs
@@ -1,12 +1,8 @@
 impl Solution {
     pub fn str_str(haystack: String, needle: String) -> i32 {
-        println!("{:?}", haystack.chars());
         for (i, c) in haystack.chars().enumerate() {
             if (i+needle.len() > haystack.len()) {break;}
-            println!("{}",i);println!("{}",c);
-            println!("{}",haystack);
             if (&haystack[i..i+needle.len()].as_bytes() == &needle.as_bytes()) {
-                println!("test");
                 return i as i32;
             }
         }
