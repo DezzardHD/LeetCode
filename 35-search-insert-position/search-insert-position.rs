5956d1327a2837cf260c66456f3c218cf1878c4c
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-02-11 15:49:58 +0100

    Time: 0 ms (100.00%) | Memory: 2.3 MB (41.96%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let mut low: usize = 0;
+        let mut high: usize = nums.len() - 1;
+        
+        while (low != high) {
+            let mut k: usize = low + (high - low + 1) / 2;
+            println!("k:{}", k);
+            if (nums[k] <= target) {
+                low = k;
+                println!("low:{}", low);
+            } else {
+                high = k - 1;
+                println!("high:{}", high);
+            }
+        }
+        if (nums[high] != target) {
+            if (nums[0] > target) {
+                high = 0;
+            } else {
+                high += 1;
+            }
+        }
+        high as i32
+    }
+}
