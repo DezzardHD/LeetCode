160f1ef155902faab4c19197732b70addbd0df1e
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-02-15 14:11:47 +0100

    Time: 189 ms (5.09%) | Memory: 4.8 MB (70.27%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+use std::vec::Vec;
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut map: HashMap<Vec<i32>, Vec<String>> = HashMap::new();
+
+        for s in strs.into_iter() {
+            let mut v: Vec<i32> = vec![0; 26];
+            for c in s.chars() {
+                v[(u32::from(c) - u32::from('a')) as usize] += 1;
+                println!("{}", c);
+            }
+            println!("{:?}", v);
+            if let Some(list) = map.get_mut(&v) {
+                list.push(s);
+            } else {
+                map.insert(v, vec![s]);
+            }
+            
+
+        }
+        println!("{:?}", map);
+        map.into_values().collect()
+    }
+}
