e4b4c813ea23b1411ca65ba356760c45cc1ddd31
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-02-21 20:56:22 +0100

    Time: 718 ms (5.49%) | Memory: 4.5 MB (45.05%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut res: Vec<i32> = vec![0; temperatures.len()];
+        let mut stack: Vec<(i32, i32)> = Vec::new();
+
+        for (i, temp) in temperatures.iter().enumerate() {
+
+            //println!("{}_{}", i, temp);
+
+            while !stack.is_empty() && *temp > (*stack.last().unwrap()).0 {
+                println!("{}_{}", i, temp);
+                let (temp2, index) = stack.pop().unwrap();
+                res[index as usize] = (i as i32) - (index as i32);
+                
+            }
+            stack.push((*temp, i as i32));
+        }
+        res
+    }
+}
