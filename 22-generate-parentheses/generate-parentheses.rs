e0e512e5050cb0d30eb06e5835ee2b75f2b35ff3
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-02-22 14:27:31 +0100

    Time: 28 ms (4.82%) | Memory: 2.4 MB (95.18%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut stack: Vec<String> = Vec::with_capacity((2*n) as usize);
+        let mut result: Vec<String> = Vec::new();
+        Solution::next(n, &mut stack, &mut result, 0, 0);
+        result
+    }
+
+    fn next(n: i32, s: &mut Vec<String>, r: &mut Vec<String>, open: i32, close: i32) {
+         println!("{:?}", s);
+         if close > open || open > n || close > n {
+            // invalid
+            return
+        }
+        if s.len() as i32 == 2*n {
+            r.push(s.join(""));
+            return
+        }
+       
+
+        s.push(String::from("("));
+        Solution::next(n, s, r, open + 1, close);
+        s.pop();
+        
+        
+        s.push(String::from(")"));
+        Solution::next(n, s, r, open, close + 1);
+        s.pop();
+    }
+}
