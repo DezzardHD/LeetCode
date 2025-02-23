b88376192e97d23227836178a104ffe5dc5182ca
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-02-23 20:35:15 +0100

    Time: 1026 ms (5.71%) | Memory: 3.9 MB (93.66%) - LeetSync

---
+++ 883-car-fleet/car-fleet.rs
@@ -0,0 +1,22 @@
+use std::iter::zip;
+
+impl Solution {
+    pub fn car_fleet(target: i32, position: Vec<i32>, speed: Vec<i32>) -> i32 {
+        let mut cars = Vec::from_iter(position.into_iter().zip(speed));
+        cars.sort();
+        let mut count = 0;
+        let mut time = 0f64;
+        for car in cars.iter().rev() {
+            let mut curr_time: f64 = (target - car.0) as f64 / car.1 as f64;
+            if time < curr_time{
+                count = count + 1;            
+                time = curr_time;
+            }
+            
+            println!("_{:?}", car);
+            println!("{}", time);
+            println!("{}", count);
+        }
+        count
+    }
+}
