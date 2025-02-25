28af020f2cdcd6c64c10192f8c80a6168e8e57ab
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-02-25 16:45:28 +0100

    Time: 30 ms (23.68%) | Memory: 20 MB (6.11%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,31 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        prefix = []
+        suffix = []
+        for i in range(0, len(height)):
+            if not prefix or height[i] >= height[prefix[-1]]:
+                if not prefix:
+                    prefix.append(i)
+                elif prefix[-1] < suffix[-1]:
+                    prefix.append(i)
+                else:
+                    break
+            if not suffix or height[len(height) - 1 - i] >= height[suffix[-1]]:
+                if not suffix:
+                    suffix.append(len(height) - 1 - i)
+                elif prefix[-1] < suffix[-1]:
+                    suffix.append(len(height) - 1 - i)
+                else:
+                    break
+
+        area = 0
+        for i in range(0, len(prefix) - 1):
+            for ix in range(prefix[i] + 1, prefix[i + 1]):
+                area += height[prefix[i]] - height[ix]
+
+        for i in range(0, len(suffix) - 1):
+            for ix in range(suffix[i] - 1, suffix[i + 1], -1):
+                print(ix)
+                area += height[suffix[i]] - height[ix]
+
+        return area
