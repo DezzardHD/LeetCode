ce8d0d7fe7821167a3c3bf4fefe35382fca4a859
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-04-05 08:43:26 +0200

    Time: 14 ms (32.72%) | Memory: 17.9 MB (40.64%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,33 +1,17 @@
 class Solution:
-    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        res = set()
-        def create_key(combination: List[int]) -> dict[int, int]:
-            a = {}
-            for val in combination:
-                a[val] = a.get(val, 0) + 1
-            return a
-
-        def explore(idx: int, value: int, current_list: List[int]):
-            nonlocal res, nums, target
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        def explore(id, current, value):
             if value == target:
-                res.add(frozenset(create_key(current_list).items()))
+                res.append(current.copy())
                 return
-            if value > target:
+            if id >= len(candidates) or value > target:
                 return
-            for i in range(idx, len(nums)):
-                tmp = value
-                tmp += nums[i]
-                tmp_list = current_list.copy()
-                tmp_list.append(nums[i])
-                explore(idx, tmp, tmp_list)
-        for i in range(0, len(nums)):                
-            explore(i, 0, [])
-        
-        real_res = []
-        for hm in res:
-            tmp = []
-            for key, val in hm:
-                tmp += [key] * val
-            real_res.append(tmp)
-        
-        return real_res
+            
+            current.append(candidates[id])
+            explore(id, current, value + candidates[id])
+            current.pop()
+            explore(id + 1, current, value)
+            return
+        explore(0, [], 0)
+        return res
