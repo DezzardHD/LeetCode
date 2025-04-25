537c707cce1926bcaa57dc0012263672083c3f2d
Moritz Franz <75507403+DezzardHD@users.noreply.github.com>
2025-04-25 22:55:50 +0200

    Time: 27 ms (3.11%) | Memory: 13.4 MB (59.93%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -7,12 +7,13 @@ class Solution(object):
         res = []
         for val in range(1, n + 1):
             print(val)
-            if val % 3 == 0 and val % 5 == 0:
-                res.append("FizzBuzz")
-            elif val % 3 == 0:
-                res.append("Fizz")
-            elif val % 5 == 0:
-                res.append("Buzz")
-            else:
+            tmp = ""
+            if val % 3 == 0:
+                tmp += "Fizz"
+            if val % 5 == 0:
+                tmp += "Buzz"
+            if tmp == "":
                 res.append(str(val))
+            else:
+                res.append(tmp)
         return res
